ec9d8799db1226dc1e81288a7b19815e58f6cbbf
jackfaarmer <jackfarmer109@gmail.com>
2025-01-20 18:33:09 -0500

    add middleware for tasks

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-//     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-//     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-//     Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-//     Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-//     Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
 
 Route::get('/tasks', function() {
     return view('tasks');
